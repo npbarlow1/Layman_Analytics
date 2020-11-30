74126059618f58e1b978ae1b9a68b52f6cb04a4f
Nathan Barlow <73715794+npbarlow1@users.noreply.github.com>
2020-11-29 21:07:51 -0800

    Create analyticsWizard.js

---
+++ analyticsWizard.js
@@ -0,0 +1,7 @@
+var analyticsWizard = confirm("Click Ok if you're not an Analytics Wizard, if you are press cancel");
+if (analyticsWizard =- true) {
+  alert("That's ok, you have come to the right place!");
+}
+  else {
+    location.href = "https://giphy.com/gifs/CurfewSeries-billy-zane-jason-thorpe-ill-cope-QugaDu0KxTtOG9RJe2"
+  
